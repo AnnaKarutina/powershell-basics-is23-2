f19dd4dd03e3c6a9cf80ac981e03d8fb646e758f
Anna Karutina <anna.karutina@voco.ee>
2023-11-16 10:01:24 +0200

    practice 1 - example solution

--- pr1.ps1
+++ pr1.ps1
@@ -4,8 +4,8 @@ $array1
 Write-Host
 $array2
 Write-Host
-$array3 = @(0, 0, 0)
-$array3[0] = $array1[0] + $array2[0]
-$array3[1] = $array1[1] + $array2[1]
-$array3[2] = $array1[2] + $array2[2]
+$array3 = @()
+$array3 = $array3 + ($array1[0] + $array2[0])
+$array3 = $array3 + ($array1[1] + $array2[1])
+$array3 += $array1[2] + $array2[2]
 $array3
